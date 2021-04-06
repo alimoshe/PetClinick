fe08b15ecb3fec3816a987a04d47434725880272
arsalehi <engsalehi@live.com>
2021-04-06 13:42:44 +0430

    Push Commit

---
+++ src/main/java/com/asalehi/petclinick/PetclinickApplication.java
@@ -0,0 +1,13 @@
+package com.asalehi.petclinick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asalehi/petclinick/PetclinickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asalehi.petclinick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
